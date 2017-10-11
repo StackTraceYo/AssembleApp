14caa24b30423c6996ecdc93e05e7697db8c03c9
Ahmad Farag <afarag@cas.org>
2017-10-11 13:07:13 -0400

    Added cache

--- assemble-web/web/app/api/content/controller/ContentController.scala
+++ assemble-web/web/app/api/content/controller/ContentController.scala
@@ -4,29 +4,40 @@ import javax.inject.{Inject, Singleton}
 
 import api.content.Request.ContentRequest
 import api.content.domain.Content
-import org.stacktrace.yo.user.auth.service.AuthTokenService
-import play.api.libs.json.Json
+import play.api.Logger
+import play.api.cache.AsyncCacheApi
+import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.{AbstractController, Action, ControllerComponents, Result}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class ContentController @Inject()(cc: ControllerComponents, tokenService: AuthTokenService)(implicit ec: ExecutionContext) extends AbstractController(cc) {
+class ContentController @Inject()(cache: AsyncCacheApi, cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc) {
 
   def index = Action {
     Ok("Content Ready.")
   }
 
   def retrieveContent: Action[ContentRequest] = Action.async(parse.json[ContentRequest]) { request =>
-    Future {
-      request.body.contentName match {
-        case "CATEGORY" =>
-          Ok(Json.toJson(Content.categories))
-        case "ALL" =>
-          Ok(Json.toJson(Content.categories))
+
+    cache.getOrElseUpdate[JsValue](request.body.contentName) {
+      Future {
+        Logger.info(s"Getting from Cache: $request.body.contentName")
+        val value =
+          request.body.contentName match {
+            case "CATEGORY" =>
+              Json.toJson(Content.categories)
+            case "ALL" =>
+              Json.toJson(Content.categories)
+          }
+        cache.set(request.body.contentName, value)
+        value
       }
-    }
+    }.map(res => {
+      Ok(res)
+    })
   }
+
 
   private def redirectToUnauthorized: Future[Result] = {
     Future {
