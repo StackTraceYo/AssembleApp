1e6182f29553ad4be0a0d6c6c1cb1689b6ff97de
Ahmad <ahmad.k.farag@gmail.com>
2017-10-14 16:30:30 -0400

    update client for group creation and list with new fields

--- client/src/app/app-routing/app-routing.module.ts
+++ client/src/app/app-routing/app-routing.module.ts
@@ -1,11 +1,9 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginFormComponent} from '../user-auth/login-form/login-form.component';
-import {UserModule} from '../user-auth/user.module';
 import {RegisterFormComponent} from '../user-auth/register-form/register-form.component';
 import {DashboardMainComponent} from '../dashboard/dashboard-main/dashboard-main.component';
 import {AuthGuard} from '../user-auth/auth-guard/auth.guard';
-import {DashboardModule} from '../dashboard/dashboard.module';
 
 const routes: Routes = [
     {path: '', redirectTo: '/login', pathMatch: 'full'},
--- client/src/app/dashboard/dashboard-my-groups/dashboard-my-groups.component.ts
+++ client/src/app/dashboard/dashboard-my-groups/dashboard-my-groups.component.ts
@@ -12,11 +12,4 @@ export class DashboardMyGroupsComponent {
 
     constructor() {
     }
-
-    tiles = [
-        {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-        {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-        {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-        {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
-    ];
 }
