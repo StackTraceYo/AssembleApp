ab2f4ca9e92e70dcc13e9d81b0cd6bca0a562e1e
Ahmad Farag <ahmad.k.farag@gmail.com>
2017-10-19 20:49:00 -0400

    Login ngrx, added state inputs for login-form component

--- client/src/app/user-auth/login-form/login-form.component.ts
+++ client/src/app/user-auth/login-form/login-form.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {LoginRequest} from '../user-api/request/login-request';
 import {Store} from '@ngrx/store';
+import * as fromAuth from '../reducers/reducers';
 import * as auth from '../reducers/auth-reducer';
 import {FormControl, FormGroup} from '@angular/forms';
 import {Login} from '../actions/auth-actions';
@@ -17,6 +18,8 @@ export class LoginFormComponent implements OnInit {
 
     appname = 'Assemble';
     loggingIn: boolean;
+    pending$ = this.store.select(fromAuth.getLoginPagePending);
+    error$ = this.store.select(fromAuth.getLoginPageError);
 
     loginForm: FormGroup = new FormGroup({
         email: new FormControl(''),
