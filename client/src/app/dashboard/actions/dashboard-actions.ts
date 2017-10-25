349b60b31e9da511d4da27cd82e1d41f33200d19
Ahmad Farag <ahmad.k.farag@gmail.com>
2017-10-24 22:59:19 -0400

    Create Group Initial NGRX

--- client/src/app/dashboard/actions/dashboard-actions.ts
+++ client/src/app/dashboard/actions/dashboard-actions.ts
@@ -3,6 +3,7 @@ import {GroupListRequest} from '../../group/group-api/request/group-list-request
 import {AssembleApiGroup} from '../../group/group-api/assemble-api-group';
 import {ContentRequest} from '../../content/content-api/request/content-request';
 import {Category} from '../../content/model/category';
+import {CreateRequest} from "../../group/group-api/request/create-request";
 
 
 export const RETRIEVE_CATEGORIES = '[Dash] Get Categories';
@@ -12,6 +13,7 @@ export const MY_GROUPS_RETRIEVED = '[Dash] Retrieved My Groups';
 export const FAILED_TO_RETRIEVE = '[Dash] Failed To Retrieve';
 export const CREATE_START = '[Dash] Starting Create Group';
 export const BACK_TO_DASH = '[Dash] Back To Dash';
+export const CREATE_GROUP = '[Dash] Creating Group';
 
 
 export class RetrieveMyGroups implements Action {
@@ -65,6 +67,16 @@ export class BackToDash implements Action {
     readonly type = BACK_TO_DASH;
 
     constructor() {
+    }
+}
+
+export class CreateGroup implements Action {
+    readonly type = CREATE_GROUP;
+
+    constructor(public payload: {
+        request: CreateRequest,
+        token: string
+    }) {
     }
 }
 
--- client/src/app/dashboard/create-group/create-group-stepper/create-group-stepper.component.ts
+++ client/src/app/dashboard/create-group/create-group-stepper/create-group-stepper.component.ts
@@ -1,13 +1,15 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {CreateRequest} from '../../../group/group-api/request/create-request';
-import {GroupApiService} from '../../../group/group-api/group-api.service';
 import {MatDialog} from '@angular/material/';
 import {CreateGroupSuccessDialogComponent} from '../create-group-success-dialog/create-group-success-dialog.component';
 import {Category} from '../../../content/model/category';
 import {Store} from '@ngrx/store';
 import * as fromCreate from '../../reducers/create-group-reducers';
 import * as dash from '../../reducers/reducers';
+import * as fromAuth from '../../../user-auth/reducers/reducers';
+import {selectAuthStatusState} from '../../../user-auth/reducers/reducers';
+import {CreateGroup} from '../../actions/dashboard-actions';
 
 @Component({
     selector: 'asm-create-group-stepper',
@@ -16,16 +18,14 @@ import * as dash from '../../reducers/reducers';
 })
 export class CreateGroupStepperComponent implements OnInit {
 
-    categories$= this.store.select(dash.selectCategories);
+    categories$ = this.store.select(dash.selectCategories);
     @Output() onCreateFinished = new EventEmitter<string>();
 
     basicInfo: FormGroup;
     category: FormGroup;
 
-    // additionalInfo: FormGroup;
-
-    constructor(private _formBuilder: FormBuilder, private groupApiService: GroupApiService, public dialog: MatDialog,
-                private store: Store<fromCreate.State>) {
+    constructor(private _formBuilder: FormBuilder, public dialog: MatDialog,
+                private store: Store<fromCreate.State>, private authStore: Store<fromAuth.State>) {
     }
 
     onCategorySelected(category: Category) {
@@ -39,24 +39,18 @@ export class CreateGroupStepperComponent implements OnInit {
         this.category = this._formBuilder.group({
             categoryName: ['']
         });
-        // this.additionalInfo = this._formBuilder.group({
-        //     category: ['', Validators.required]
-        // });
     }
 
     create() {
-        const request = new CreateRequest();
-        request.groupName = this.basicInfo.value.groupName;
-        request.categoryName = this.category.value.categoryName;
-        const createOp = this.groupApiService.create(request);
-        createOp.subscribe(
-            created => {
-                console.log(created.groupId);
-                this.showSuccess();
-            },
-            err => {
-                console.log(err);
-            });
+        this.authStore.select(selectAuthStatusState).subscribe(state => {
+                if (state.authenticated) {
+                    const request = new CreateRequest();
+                    request.groupName = this.basicInfo.value.groupName;
+                    request.categoryName = this.category.value.categoryName;
+                    this.store.dispatch(new CreateGroup({request: request, token: state.token}));
+                }
+            }
+        );
     }
 
     showSuccess(): void {
